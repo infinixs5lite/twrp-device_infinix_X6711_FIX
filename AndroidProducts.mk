9242e4bb2b93fc0bdb686e10d8ca1fffe1957510
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-08-16 23:02:39 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,4 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_X6711.mk
 
 COMMON_LUNCH_CHOICES := \
+    twrp_X6711-user \
+    twrp_X6711-userdebug \
     twrp_X6711-eng
